32977af9ce68c304e8261969cda69d1ff4350dbe
rafal <rafal@WIN-KKRO09GL1GC>
2016-11-27 23:21:46 +0100

    Add project files.

---
+++ CardReader/MainWindow.xaml.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Threading;
+using PCSC;
+
+namespace CardReader
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public IList<string> Readers { get; set; }
+        public IList<string> Logs { get; set; }
+
+        public string CurrentReader
+        {
+            get { return _currentReader; }
+            set
+            {
+                _currentReader = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Status
+        {
+            get { return _status; }
+            set
+            {
+                _status = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private readonly SCardReader _sCardReader;
+        private IntPtr _protocol;
+        private readonly SCardContext _sCardContext;
+        private string _status;
+        private string _currentReader;
+
+        public MainWindow()
+        {
+            Dispatcher.UnhandledException += LogError;
+            DataContext = this;
+
+            Readers = new ObservableCollection<string>();
+            Logs = new ObservableCollection<string>();
+            Status = "NotConnected";
+
+            _sCardContext = new SCardContext();
+            _sCardContext.Establish(SCardScope.System);
+            _sCardReader = new SCardReader(_sCardContext);
+
+            InitializeComponent();
+        }
+
+        private void LogError(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            Log("Error. Exception: {0} with message: {1}", e.Exception.GetType().Name, e.Exception.Message);
+            e.Handled = true;
+        }
+
+        private void LoadReaders(object sender, EventArgs e)
+        {
+            var readers = _sCardContext.GetReaders();
+
+            Log("Card readers loaded.");
+
+            if (readers.Length > 0)
+            {
+                CurrentReader = readers[0];
+            }
+
+            Readers.Clear();
+
+            foreach (var reader in readers)
+            {
+                Readers.Add(reader);
+            }
+        }
+
+        private void ConnectCard(object sender, EventArgs e)
+        {
+            var result = _sCardReader.Connect(CurrentReader,
+                SCardShareMode.Shared,
+                SCardProtocol.Any);
+
+            Status = result.ToString();
+
+            Log("Card connect attempt: " + Status);
+
+            if (result == SCardError.Success)
+            {
+                SetProtocol();
+            }
+        }
+
+        private void SetProtocol()
+        {
+            switch (_sCardReader.ActiveProtocol)
+            {
+                case SCardProtocol.T0:
+                    _protocol = SCardPCI.T0;
+                    break;
+                case SCardProtocol.T1:
+                    _protocol = SCardPCI.T1;
+                    break;
+                default:
+                    throw new PCSCException(SCardError.ProtocolMismatch, "Not supported protocol: " + _sCardReader.ActiveProtocol.ToString());
+            }
+
+            SendCommand(new byte[] { 0xA0, 0xA4, 0x00, 0x00, 0x02, 0x7F, 0x10 }); //przykladowa komenda
+        }
+
+        private void Log(string log, params object[] param)
+        {
+            Logs.Insert(0, string.Format(log + " #" + DateTime.Now, param));
+        }
+
+        private void SendCommand(byte[] command)
+        {
+            Log("Sending command: {0}", string.Join(" ", command.Select(x => string.Format("{0:X2} ", x))));
+
+            byte[] response = new byte[256];
+            _sCardReader.Transmit(_protocol, command, ref response);
+
+            Log("Command response: {0}", string.Join(" ", response.Select(x => string.Format("{0:X2} ", x))));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _sCardContext.Dispose();
+            base.OnClosed(e);
+        }
+    }
+}
